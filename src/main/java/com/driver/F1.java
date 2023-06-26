15cb773f7bc24f7f8641ac4ea18b46e9b6d4558e
vibhanshu08 <108286891+vibhanshu08@users.noreply.github.com>
2023-06-26 17:28:17 +0530

    Update F1.java

--- src/main/java/com/driver/F1.java
+++ src/main/java/com/driver/F1.java
@@ -4,6 +4,8 @@ public class F1 extends Car {
 
     public F1(String name, boolean isManual) {
         //Use arbitrary values for parameters which are not mentioned
+        Vehicle(name);
+        this.isManual=isManual;
         
     }
 
